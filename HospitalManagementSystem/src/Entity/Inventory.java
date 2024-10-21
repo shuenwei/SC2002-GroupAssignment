5419c7585a761f58c85de857c953d35cd6805235
Yun Kar Kit <yunkarkit787@gmail.com>
2024-10-21 22:13:05 +0800

    added implementation for Inventory Medication PrescribedMedication Request classes

--- HospitalManagementSystem/src/Entity/Inventory.java
+++ HospitalManagementSystem/src/Entity/Inventory.java
@@ -1,4 +1,19 @@
 package Entity;
 
+import java.util.HashMap;
+
 public class Inventory {
+    private static HashMap<String, Medication> medicineRepo = new HashMap<String, Medication>();
+
+    public static void add(Medication medicine) {
+        medicineRepo.put(medicine.getMedicineName(), medicine);
+    }
+    public static Medication get(String medicineName) {
+        return medicineRepo.get(medicineName);
+    }
+    public static void remove(String medicineName) {
+        if (medicineRepo.get(medicineName) != null) {
+            medicineRepo.remove(medicineName);
+        }
+    }
 }
--- HospitalManagementSystem/src/Entity/Request.java
+++ HospitalManagementSystem/src/Entity/Request.java
@@ -1,4 +1,23 @@
 package Entity;
 
+import Enums.RequestStatus;
+
 public class Request {
+    private String medicineName;
+    private RequestStatus status;
+
+    public Request(String medicineName) {
+        this.medicineName = medicineName;
+        this.status = RequestStatus.PENDING;
+    }
+    
+    public String getRequestMedicine() {
+        return this.medicineName;
+    }
+    public RequestStatus getStatus() {
+        return this.status;
+    }
+    public void setStatus(RequestStatus status) {
+        this.status = status;
+    }
 }
