8e58f46cd117c0250a1024fc9e38d9879b1dfa98
Yun Kar Kit <yunkarkit787@gmail.com>
2024-10-22 16:04:52 +0800

    added function for pharmacist to submit replenishment request

--- HospitalManagementSystem/src/Controller/PharmacistController.java
+++ HospitalManagementSystem/src/Controller/PharmacistController.java
@@ -1,6 +1,9 @@
 package Controller;
 
 import Entity.Pharmacist;
+import Entity.Inventory;
+import Entity.Request;
+import Entity.Medication;
 
 public class PharmacistController {
     private Pharmacist pharmacist;
@@ -9,4 +12,20 @@ public class PharmacistController {
         this.pharmacist = pharmacist;
     }
 
+    public static void submitRequest(String requestMedicine) {
+        Request newRequest = new Request(requestMedicine);
+        if (requestMedicine != null) {
+            Medication medicine = Inventory.get(requestMedicine);
+            if (medicine.getIsLowStock() != "YES") {
+                System.out.printf("%s is not low on stock%n", requestMedicine);
+            }
+            else {
+                Inventory.addRequest(newRequest);
+                System.out.printf("Successfully submitted replenishment request for %s%n", requestMedicine);
+            }
+        }
+        else {
+            System.out.println("Medicine does not exist!");
+        }
+    }
 }
